cb3f1f6c90e5e44f33bbb00ff2d7db6ac499497a
Sharad Vikram <sharadmv@fb.com>
2013-07-19 23:41:30 -0700

    got config working again

--- server/config.js
+++ server/config.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+
 var DB_CONFIG = JSON.parse(fs.readFileSync(__dirname+'/../config/db.json'));
 var EMAIL_CONFIG = JSON.parse(fs.readFileSync(__dirname+'/../config/email.json'));
 var MEMCACHED_CONFIG = JSON.parse(fs.readFileSync(__dirname+'/../config/memcached.json'));
